c35dee76b10d424624b91e0790c99ad57787b242
Lusca Lima <lucas@siasp.com.br>
2020-07-15 21:23:52 -0300

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-toolbar app color="#e10098" dark>
+      <v-img :src="require('@/assets/gql_logo.svg')" max-width="40"></v-img>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>GraphQL</span>
+        <span class="font-weight-light">Client</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-content>
+      <Conteudo />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+import Conteudo from "./componentes/Conteudo";
+
+export default {
+  name: "App",
+  components: { Conteudo },
+  methods: mapActions(["setUsuario"]),
+  created() {
+    this.setUsuario(null);
+  }
+};
+</script>
---
+++ src/componentes/autenticacao/UsuarioLogado.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card v-if="usuario" class="ma-2 pa-1">
+    <v-layout fill-height align-center>
+      <v-flex>
+        <span class="ml-3 body-1">Nome:{{' '}}</span>
+        <span class="body-1 blue--text text--darken-2">{{ usuario.nome }}</span>
+
+        <span class="ml-3 body-1">Email:{{' '}}</span>
+        <span class="body-1 blue--text text--darken-2">{{ usuario.email }}</span>
+
+        <span class="ml-3 body-1">Perfis:{{' '}}</span>
+        <span class="body-1 red--text text--darken-2">{{ perfis }}</span>
+      </v-flex>
+      <v-flex></v-flex>
+      <v-flex shrink>
+        <v-btn color="error" @click="setUsuario(null)" small flat>Sair</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-card>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["usuario"]),
+    perfis() {
+      if (!this.usuario && !this.usuario.perfis) return null;
+      return this.usuario.perfis.map(p => p.rotulo).join(", ");
+    }
+  },
+  methods: mapActions(["setUsuario"])
+};
+</script>
+
+<style>
+</style>
